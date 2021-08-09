22a6b6d8bd3d6c16d64503b42c953635a183d37e
Andrew Enger <andrew.enger48@gmail.com>
2021-08-08 23:20:54 -0400

    test

--- bot.py
+++ bot.py
@@ -1,28 +1,39 @@
 import discord
 import os
 import json
-from web3 import Web3
-from web3.gas_strategies.time_based import slow_gas_price_strategy, medium_gas_price_strategy, fast_gas_price_strategy
-
-w3 = Web3(Web3.HTTPProvider(os.getenv('ETH_API')))
+import requests
+import time
+import asyncio
+from dotenv import load_dotenv
 
 client = discord.Client()
 
+load_dotenv()
+
 def get_gas_prices():
-    w3.eth.set_gas_price_strategy(slow_gas_price_strategy)
-    slow = w3.toWei(w3.eth.generate_gas_price(), 'gwei')
+  r = requests.get(f"https://data-api.defipulse.com/api/v1/egs/api/ethgasAPI.json?api-key={os.getenv('API_KEY')}")
+  try:
+    r.raise_for_status()
+    gas_prices = r.json()
+    gas_dict = {
+      "low": int(gas_prices['safeLow']) / 10,
+      "medium": int(gas_prices['fast']) / 10,
+      "high": int(gas_prices['fastest']) / 10,
+      "average": int(gas_prices['average']) / 10
+    }
+    return gas_dict
+  except Exception as e:
+    return "Error fetching gas prices"
 
-    w3.eth.set_gas_price_strategy(medium_gas_price_strategy)
-    med = w3.toWei(w3.eth.generate_gas_price(),'gwei')
-
-    w3.eth.set_gas_price_strategy(fast_gas_price_strategy)
-    fast = w3.toWei(w3.eth.generate_gas_price(),'gwei')
-
-    return (slow, med, fast)
+async def update_presence():
+    prices = get_gas_prices()
+    await client.change_presence(activity=discord.Game(f"🐌 {prices['low']} | 🚶 {prices['medium']} | 🔥 {prices['high']}"))
+    await asyncio.sleep(15)
 
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
+  client.loop.create_task(update_presence())
 
 @client.event
 async def on_message(message):
@@ -32,10 +43,11 @@ async def on_message(message):
   if message.content.startswith('!gas'):
     prices = get_gas_prices()
     embed = discord.Embed(title="Gas Prices")
-    embed.add_field(name="Slow Gas", value=prices[0])
-    embed.add_field(name="Medium Gas", value=prices[1])
-    embed.add_field(name="Fast Gas", value=prices[2])
-    await message.send(embed=embed)
-    
+    embed.add_field(name=":snail: Slow :snail:", value=prices['low'], inline=False)
+    embed.add_field(name=":person_walking: Medium :person_walking:", value=prices['medium'], inline=False)
+    embed.add_field(name=":fire: Fast :fire:", value=prices['high'], inline=False)
+    embed.add_field(name=":blue_circle: Average :blue_circle:", value=prices['average'], inline=False)
+    await message.channel.send(embed=embed)
+
 if __name__ == "__main__":
   client.run(os.getenv('TOKEN'))
