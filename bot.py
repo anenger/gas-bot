3040322a3928130aae166263e9e3073b2ebb57d8
Andrew Enger <andrew.enger48@gmail.com>
2021-08-08 20:00:16 -0400

    shit

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 import os
 import json
-from web3 import Web3, middleware
+from web3 import Web3
 from web3.gas_strategies.time_based import slow_gas_price_strategy, medium_gas_price_strategy, fast_gas_price_strategy
 
 w3 = Web3(Web3.HTTPProvider(os.getenv('ETH_API')))
@@ -9,10 +9,6 @@ w3 = Web3(Web3.HTTPProvider(os.getenv('ETH_API')))
 client = discord.Client()
 
 def get_gas_prices():
-    w3.middleware_onion.add(middleware.time_based_cache_middleware)
-    w3.middleware_onion.add(middleware.latest_block_based_cache_middleware)
-    w3.middleware_onion.add(middleware.simple_cache_middleware)
-
     w3.eth.set_gas_price_strategy(slow_gas_price_strategy)
     slow = w3.toWei(w3.eth.generate_gas_price(), 'gwei')
 
